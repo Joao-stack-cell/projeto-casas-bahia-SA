5dedd4730d5b3431ebfef93948955f07ce6238ec
joao-stack-cell <camposjoaopaulo1@gmail.com>
2024-07-19 22:35:58 -0300

    Quarto commit-Projeto Casas Bahia

---
+++ script.js
@@ -0,0 +1,19 @@
+function  trocaCor(cor) {
+let amarelo = document.querySelector('.amarelo');
+amarelo.style.background = cor;
+}
+
+
+function trocaIphone(imagem) {
+
+let iphone = document.querySelector('.iphone');
+iphone.src = imagem
+
+
+}
+
+
+
+
+
+
